c96f17a06924016ed9d7c52cda34b14cd9faa7e6
Rafi Gil Diaz <lego.nut@hotmail.com>
2020-07-29 13:24:56 -0500

    Create solve.py

---
+++ solve.py
@@ -0,0 +1,25 @@
+### Generates a list of moves to make in order to solve the Rubik's Cube from a given scramble.
+
+## Imports
+from .solve import *
+from .cube import *
+from copy import copy
+from math import inf
+
+## Settings **TO BE IMPLEMENTED**
+# Pass this to Solve().
+settings = {
+    # Solution method (beginner's method, cfop, roux, zz, petrus)
+    'METHOD': 1,
+    # Number of solutions to output. Defaults to all that are given.
+    'NUM_OUT': inf
+}
+
+class Solve():
+    # Initializes a solve.
+    # @param scrambles The list of scrambles to solve.
+    def __init__(self, scrambles):
+        self.scrambles = scrambles
+        self.settings = copy(settings)
+        self.results = []
+    
