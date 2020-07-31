a35db2da65bfe3d6d7468e767a067f194a0788af
Rafi Gil Diaz <lego.nut@hotmail.com>
2020-07-31 12:13:08 -0500

    Updates scrambleParse.py and add debugging

--- scramble/scrambleParse.py
+++ scramble/scrambleParse.py
@@ -1,4 +1,4 @@
-from .scramble import Scramble
+from scramble import Scramble
 
 
 class ScrambleParse:
@@ -7,20 +7,34 @@ class ScrambleParse:
     def __init__(self, unparsed):
         self.unparsed = unparsed
         self.scrambles = self.parse(unparsed)
-        self.numScrambles = self.scrambles.length
+        self.numScrambles = len(self.scrambles)
 
     # Returns a string showing the unparsed scramble and number of scrambles given.
     def __str__(self):
-        return f'<ScrambleParse: {self.numScrambles} {self.unparsed}>'
+        return f'<ScrambleParse: {self.numScrambles} "{self.unparsed}" {self.scrambles}>'
 
+    # Parses the unparsed string of scrambles and returns a list of Scramble objects.
     def parse(self, unparsed):
         if not isinstance(unparsed, str):
             raise TypeError(f'unparsed must be a str. Given: {unparsed} of type {type(unparsed)}')
         s = unparsed.split('/')
+        index = 0
         for i in s:
-            i.strip()
+            i = Scramble(i.strip(), index)
+            print(i)
+            index += 1
         return s
+
+    def get_unparsed(self):
+        return self.unparsed
+
+    def get_scrambles(self):
+        return self.scrambles
+    
+    def get_numScrambles(self):
+        return self.numScrambles
 
 # Debugging
 if __name__ == "__main__":
-    ScrambleParse("     R F U' x / R F  ")
+    x = ScrambleParse("     R F U' x / R F  ")
+    print(x.get_scrambles())
