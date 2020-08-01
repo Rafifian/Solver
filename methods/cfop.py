8cb6177324757594fd58aff3a7269fc6cd257ea8
Rafi Gil Diaz <lego.nut@hotmail.com>
2020-07-31 20:07:17 -0500

    cfop framework

--- methods/cfop.py
+++ methods/cfop.py
@@ -1,1 +1,12 @@
 ## The steps for solving a cube with CFOP
+
+class CFOP:
+    def __init__(self, cube):
+        self.cube = cube
+        self.solution = []
+
+        self.solve()
+
+    def solve(self):
+        cube = self.cube
+        solution = self.solution
