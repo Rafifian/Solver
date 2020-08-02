910f225c5c226c46f6067d772411843b814edcc9
Rafi Gil Diaz <lego.nut@hotmail.com>
2020-08-01 19:32:11 -0500

    Add cfop structure

--- methods/cfop.py
+++ methods/cfop.py
@@ -1,4 +1,6 @@
 ## The steps for solving a cube with CFOP
+# Imports
+from cube import Cube
 
 class CFOP:
     def __init__(self, cube):
@@ -10,6 +12,20 @@ class CFOP:
     def solve(self):
         cube = self.cube
         solution = self.solution
+
+        self.cross(cube, solution)
+    
+    def cross(self, cube, solution):
+        pass
+
+    def f2l(self, cube, solution):
+        pass
+
+    def oll(self, cube, solution):
+        pass
+
+    def pll(self, cube, solution):
+        pass
 
 # Debugging
 if __name__ == "__main__":
