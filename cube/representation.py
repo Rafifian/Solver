770899ad0efeaa7c251b36899dfc8d2f05ce74a6
Rafi Gil Diaz <rfgildiaz@gmail.com>
2020-10-02 16:49:29 -0500

    implement rTurn() and rotate()

--- cube/representation.py
+++ cube/representation.py
@@ -66,6 +66,20 @@ solved_state = [
         [51, 52, 53]
     ],
 ]
+
+blank_state = [[[-1 for y in range(3)]
+                for x in range(3)]
+                for z in range(6)]
+
+faces = {
+    'u': 0,
+    'l': 1,
+    'f': 2,
+    'r': 3,
+    'b': 4,
+    'd': 5
+}
+
 # A scrambled state will have numbers in different places, according to the turn methods that can be found below.
 # 
 # Importantly, numbers 4, 13, 22, 31, 40, and 49 will never move, as the centers of each face stay the same in relation to each other.
@@ -77,14 +91,102 @@ class Cube:
     ## Representation of a Rubik's cube
     def __init__(self):
         self.cube = copy(solved_state)
+        self.blank_state = copy(blank_state)
 
     # Checks whether or not the cube is solved.
     def is_solved(self):
         return self.cube == solved_state
+
+    # Rotates a 2d array clockwise
+    def rotate(self, array):
+        a = array
+        # transpose
+        for i in range(len(a)):
+            for j in range(i, len(a)):
+                a[i][j], a[j][i] = a[j][i], a[i][j]
+
+        # reverse rows
+        for i in range(len(a)):
+            k = len(a) - 1
+            for j in range(k):
+                a[i][j], a[i][k] = a[i][k], a[i][j]
+                k = k - 1
+        
+        return(a)
     
     ## Turns
     def rTurn(self):
-        pass
+        cube = self.cube
+        temp = self.blank_state
+
+        # indicies for face, row, and piece
+        i = 0
+        j = 0
+        k = 0
+
+        # for face in cube
+        for a in cube:
+            j = 0
+            # check face
+            if i == faces['u']:
+                # for row in face
+                for b in a:
+                    k = 0
+                    # for "sticker" in row
+                    for c in b:
+                        if k <= 1:
+                            temp[i][j][k] = c
+                        else: 
+                            temp[faces['b']][2-j][2-k] = c
+                        k += 1
+                    j += 1
+            elif i == faces['l']:
+                # this face isn't affected, so just copy elements from cube to temp
+                for b in a:
+                    k = 0
+                    for c in b:
+                        temp[i][j][k] = c
+                        k += 1
+                    j += 1
+            elif i == faces['f']:
+                for b in a:
+                    k = 0
+                    for c in b:
+                        if k <= 1:
+                            temp[i][j][k] = c
+                        else:
+                            temp[faces['u']][j][k] = c
+                        k += 1
+                    j += 1
+            elif i == faces['r']:
+                # rotate face
+                temp[i] = self.rotate(cube[i])
+            elif i == faces['b']:
+                for b in a:
+                    k = 0
+                    for c in b:
+                        if k >= 1:
+                            temp[i][j][k] = c
+                        else:
+                            temp[faces['d']][2-j][2-k] = c
+                        k += 1
+                    j += 1
+            elif i == faces['d']:
+                for b in a:
+                    k = 0
+                    for c in b:
+                        if k <= 1:
+                            temp[i][j][k] = c
+                        else:
+                            temp[faces['f']][j][k] = c
+                        k += 1
+                    j += 1
+            else:
+                raise AssertionError(f"{i} is not a valid face value")
+            i += 1
+
+        # set self to the temp cube we filled
+        self.cube = temp
 
     def lTurn(self):
         pass
@@ -112,4 +214,6 @@ class Cube:
 
 # Debugging
 if __name__ == "__main__":
-    pass
+    x = Cube()
+    x.rTurn()
+    print(x.cube)
